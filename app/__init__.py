88b59656f3dbb4eafe26d0f7ad0752ad989ac0c3
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2016-04-05 11:00:08 -0300

    New project structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 #!/usr/local/bin python3
 # -*- encoding: utf-8 -*-
-from flask import Flask
+from flask import Flask, send_file
 
 
-app = Flask(__name__)
+app = Flask(
+        __name__,
+        static_url_path="/static",
+        static_folder="./build"
+)
+
+@app.route("/")
+def index():
+    return send_file("./index.html")
--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 #!/usr/local/bin python3
 # -*- encoding: utf-8 -*-
+import os
 from app import app
-app.run(host="0.0.0.0", port=8000, debug=True)
+
+if __name__ == "__main__":
+    PORT = os.getenv("PORT", default=8080)
+    app.run(
+        host="0.0.0.0",
+        port=PORT,
+        debug=True
+    )
