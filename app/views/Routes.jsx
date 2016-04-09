b4e97644ca1573ab6e13a654ed9dff86333d23a0
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2016-04-09 15:08:23 -0300

    Modularization...

--- app/views/Routes.jsx
+++ app/views/Routes.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Route  } from "react-router";
+import { Route, IndexRoute  } from "react-router";
 
+import Main from "./components/Main.jsx"
 import Home from "./components/Home.jsx";
 
 module.exports = (
-    <Route path="/" component={Home}>
-
+    <Route path="/" component={Main}>
+        <IndexRoute component={Home} />
     </Route>
 )
 
---
+++ app/views/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./NavBar.jsx";
+import Footer from "./Footer.jsx";
+
+export default class App extends React.Component {
+	render() {
+        return (
+            <div>
+                <NavBar />
+                { this.props.children }
+                <Footer />
+            </div>
+       );
+	}
+}
