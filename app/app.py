99108c77b7f46af41bc40c948cd911e57800152e
ivanwolf15 <ivanwolf15gmail.com>
2016-04-09 14:16:20 -0300

    aaa

--- app/app.py
+++ app/app.py
@@ -1,15 +1,18 @@
 #!/usr/local/bin python3
 # -*- encoding: utf-8 -*-
 import smp
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from json import loads
 
+DEBUG = True
+SECRET_KEY = 'this is a secret'
 
 app = Flask(
         __name__,
         static_url_path="/static",
         static_folder="./build"
 )
+app.config.from_object(__name__)
 
 
 @app.route('/solve', METHODS=['GET'])
@@ -20,4 +23,9 @@ def black_box():
     """
     Aca va la caja negra
     """
-    return render_template('result.html', result=result)
+
+    result = dict()
+    return jsonify(**result)
+
+if __name__ == '__main__':
+    app.run()
