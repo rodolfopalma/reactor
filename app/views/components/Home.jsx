542d14c0f253c26c4a77de9d65fad7742e88047f
Polo <lsvargas@github.com>
2016-04-09 15:27:21 -0300

    actualizacion styl y Home

--- app/views/components/Home.jsx
+++ app/views/components/Home.jsx
@@ -7,7 +7,13 @@ export default class App extends React.Component {
 
               <div className="row">
                 <div className="col-md-6">
-                  <img src={'http://imgur.com/HCuv5c1'} />
+                  <img src={'http://i.imgur.com/hAXh1t7.jpg?1'} style={{
+					            	width: 450,
+					            	height: 300,
+					            	backgroundColor: 'transparent',
+					            	marginRight: 10,
+          				}} />
+
                 </div>
                     <div className="col-md-6">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget ante et massa consequat convallis. Nulla ac quam porttitor, vehicula mi at, tempor eros. Vestibulum euismod nisl et blandit semper. Nulla quis nulla risus. Maecenas fringilla suscipit ullamcorper. Ut ac varius tellus, venenatis dictum ante. Duis id lacinia tortor, maximus tristique sapien. Duis tortor velit, ornare vel blandit ac, condimentum ut urna. Cras malesuada quis purus non sollicitudin. Morbi mollis aliquet mauris, et elementum purus rhoncus in. Integer fermentum orci diam, at malesuada ligula facilisis non. Proin molestie, erat non eleifend molestie, justo ipsum vulputate lacus, vel lobortis ligula nunc quis magna. Mauris non dolor vel elit fermentum fringilla. Etiam dictum tristique fermentum. Maecenas et scelerisque eros, sed malesuada turpis. Nunc vehicula pretium auctor.
@@ -15,8 +21,8 @@ export default class App extends React.Component {
                 </div>
 
                 <div className="row">
-                    <div className="col-md-12" id="boton">
-                        <button type="button" className="btn btn-primary btn-lg">Comenzar</button>
+                    <div className="col-md-12" id="landingButton">
+                        <button type="button" className="btn btn-primary btn-lg">Begin</button>
                     </div>
                 </div>
 
