92918354f284d831108dbc954b7b1795f87fd921
Polo <lsvargas@github.com>
2016-04-09 16:59:21 -0300

    actualizacion01 main footer

--- app/views/components/Footer.jsx
+++ app/views/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default class Footer extends React.Component {
     render() {
         return (
-          <nav className="navbar navbar-default">
+          <nav className="navbar navbar-default" id="foot">
               <div className="container">
                   <h4>Facebook Chile Regional Hackathon 2016</h4>
                   <h5>Copyright All rights reserved</h5>
