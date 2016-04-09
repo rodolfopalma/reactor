622ccc04e543d99f9cee80800c76b5790c55e446
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2016-04-09 13:32:33 -0300

    Using bootstrap...

--- app/views/components/NavBar.jsx
+++ app/views/components/NavBar.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default class NavBar extends React.Component {
     render() {
         return (
-            <h1> Navbar... </h1>
+            <nav className="navbar navbar-default">
+                <div className="container">
+                    <h3>Pick a Car</h3>
+                </div>
+            </nav>
        )
     }
 }
