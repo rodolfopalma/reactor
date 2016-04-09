802fef254a1deaeed486a10463ba2d67f8460e9e
Polo <lsvargas@github.com>
2016-04-09 14:46:16 -0300

    actualizacion01 Home

--- app/views/components/Home.jsx
+++ app/views/components/Home.jsx
@@ -7,7 +7,24 @@ export default class App extends React.Component {
         return (
             <div>
                 <NavBar />
-                // Acá haz el resto Polo...
+								<div className="container">
+
+								  <div className="row">
+								    <div className="col-md-6">
+								      <img src={'http://imgur.com/HCuv5c1'} />
+								    </div>
+										<div className="col-md-6">
+								      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget ante et massa consequat convallis. Nulla ac quam porttitor, vehicula mi at, tempor eros. Vestibulum euismod nisl et blandit semper. Nulla quis nulla risus. Maecenas fringilla suscipit ullamcorper. Ut ac varius tellus, venenatis dictum ante. Duis id lacinia tortor, maximus tristique sapien. Duis tortor velit, ornare vel blandit ac, condimentum ut urna. Cras malesuada quis purus non sollicitudin. Morbi mollis aliquet mauris, et elementum purus rhoncus in. Integer fermentum orci diam, at malesuada ligula facilisis non. Proin molestie, erat non eleifend molestie, justo ipsum vulputate lacus, vel lobortis ligula nunc quis magna. Mauris non dolor vel elit fermentum fringilla. Etiam dictum tristique fermentum. Maecenas et scelerisque eros, sed malesuada turpis. Nunc vehicula pretium auctor.
+										</div>
+									</div>
+
+									<div className="row">
+										<div className="col-md-12" id="boton">
+											<button type="button" className="btn btn-primary btn-lg">Comenzar</button>
+										</div>
+									</div>
+
+								</div>
                 <Footer />
             </div>
        );
