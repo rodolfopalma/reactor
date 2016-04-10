4538b09d1c0a35b2630403fd0adb735286be33fc
Polo <lsvargas@github.com>
2016-04-10 01:20:26 -0300

    matching y texinput

--- app/views/components/Matching.jsx
+++ app/views/components/Matching.jsx
@@ -2,18 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {Table, Column, Cell} from "fixed-data-table";
 
-const rows = [
-  ['a1', 'b1', 'c1'],
-  ['a2', 'b2', 'c2'],
-  ['a3', 'b3', 'c3'],
-  // .... and more
-];
+
 
 var Matching  = React.createClass({
   render(){
-      return (
-        <h1> Matching... </h1>
-     )    
+    var rows = []
+    var drivers = Window.PAC.roles.drivers
+    var passengers = Window.PAC.roles.passengers
+    var currentUserId = Window.PAC.currentUserId
+
+    for(var i = 0; i < drivers.length; i++) {
+      currentDriver = this.state.drivers[i];
+      if currentUserId !== currentDriver.id {
+      rows.push(<TextInput key={i} name={currentDriver.name} />)
+      }
+    }
+
+    for(var i = 0; i < passengers.length; i++) {
+      currentDriver = this.state.passengers[i];
+      if currentUserId !== currentDriver.id {
+      rows.push(<TextInput key={i} name={currentDriver.name} />)
+      }
+    }
+
+
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  { rows }
+                </div>
+              </div>
+            </div>
+        );
   }
 
 });
---
+++ app/views/components/TextInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+var TextInput = React.createClass({
+    render(){
+        return(
+          <div class="form-group">
+            <label htmlFor="usr">{this.props.name}:</label>
+            <input type="text" className="form-control" id="usr">
+          </div>
+      )
+    }
+});
