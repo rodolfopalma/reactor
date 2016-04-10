5d32d982f979932a3836f8754efff9e21fada68d
Polo <lsvargas@github.com>
2016-04-10 03:23:50 -0300

    yolo

--- app/views/Routes.jsx
+++ app/views/Routes.jsx
@@ -6,7 +6,6 @@ import Home from "./components/Home.jsx"
 import Begin from "./components/Begin.jsx"
 import Matching from "./components/Matching.jsx"
 import Results from "./components/Results.jsx";
-import Results2 from "./components/Results2.jsx"
 import FBLogin from "./components/FBLogin.jsx";
 
 module.exports = (
@@ -15,7 +14,6 @@ module.exports = (
         <Route path="choose-people" component={Begin} />
         <Route path="matching" component={Matching} />
         <Route path="results" component={Results} />
-        <Route path="test" component={Results2} />
         <IndexRoute component={Home} />
     </Route>
 )
